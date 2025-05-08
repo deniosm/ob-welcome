adb84d6f4c4d04d0f9b9702df44933c670dae1f4
tvrtko-linux <76059129+deniosm@users.noreply.github.com>
2025-05-08 20:01:54 +0200

    update translations, update remove .desktop

--- usr/bin/ob-welcome-first.sh
+++ usr/bin/ob-welcome-first.sh
@@ -8,3 +8,10 @@ if [ "$(cat "$CONFIG_FILE")" = "autostart=1" ]; then
     echo "autostart=0" > "$CONFIG_FILE"
     rm -f "$AUTOSTART_FILE"
 fi
+
+# Remove ob-welcome autostart file for all users if it exists
+for file in /home/*/.config/autostart/ob-welcome-first.desktop; do
+    if [ -f "$file" ]; then
+        rm -f "$file"
+    fi
+done
